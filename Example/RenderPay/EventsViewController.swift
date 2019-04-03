2e90f9833a85921a7c6f39658cae0ed46185f08e
bobbyren <bobbyren@gmail.com>
2019-04-03 07:32:13 -0400

    added tab bar and eventsViewController

---
+++ Example/RenderPay/EventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventsViewController.swift
+//  RenderPay_Example
+//
+//  Created by Bobby Ren on 4/3/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
